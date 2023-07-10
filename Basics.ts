2c9120fe4dfd49e97042906d8a570a40281af134
MichaelLin12 <michael.lin2032@gmail.com>
2023-07-10 18:09:34 -0500

    change setup

---
+++ Basics.ts
@@ -0,0 +1,18 @@
+console.log('Your code goes here...')
+
+// Core Types
+
+/** 
+* Integer 1,5.3,-10
+* String "Hi", 'Hi', `Hi`
+* Boolean true, false
+* */
+
+function add(n1: number, n2: number){
+    return n1+n2;
+}
+
+const number1 = 5;
+const number2 = 2.8;
+const result = add(number1,number2);
+console.log(result);
